5d37adcf9aea91b1f14479b7d63f3e4b388d31ac
Taras Tarasenko <myavailability.taras@gmail.com>
2020-06-05 23:05:09 +0300

    Created DashboardTabBarController Items

--- ANODA_test/DashboardTabBarController.swift
+++ ANODA_test/DashboardTabBarController.swift
@@ -8,22 +8,45 @@
 
 import UIKit
 
-class DashboardTabBarController: UITabBarController {
-
+final class DashboardTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //MARK: - HomeViewController
+        let homeVC = HomeViewController()
+        let homeItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_home_black_18dp"), selectedImage: #imageLiteral(resourceName: "baseline_home_black_18dp"))
+        homeVC.tabBarItem = homeItem
+        
+        //MARK: - SearchViewController
+        let searchVC = UIViewController()
+        let searchItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "baseline_search_black_18dp-1"), selectedImage: #imageLiteral(resourceName: "baseline_search_black_18dp-1"))
+        searchVC.tabBarItem = searchItem
+        
+        //MARK: - AddNewViewController
+        let addPhotoVC = UIViewController()
+        let addPhotoItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_add_box_black_18dp"), selectedImage: #imageLiteral(resourceName: "baseline_add_box_black_18dp"))
+        addPhotoVC.tabBarItem = addPhotoItem
+        
+        //MARK: - FavouritesViewController
+        let favouritesVC = UIViewController()
+        let favouritesItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "baseline_favorite_border_black_18dp"), selectedImage: #imageLiteral(resourceName: "baseline_favorite_black_18dp"))
+        favouritesVC.tabBarItem = favouritesItem
+        
+        //MARK: - ProfileViewController
+        let profileVC = UIViewController()
+        let profileItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "outline_menu_black_18dp"), selectedImage: #imageLiteral(resourceName: "outline_menu_black_18dp"))
+        profileVC.tabBarItem = profileItem
+        
+        self.viewControllers = [homeVC, searchVC, addPhotoVC, favouritesVC, profileVC]
     }
     
-
-    override func viewWillAppear(_ animated: Bool) {
-          super.viewWillAppear(animated)
-          let homeVC = HomeViewController()
-        let icon1 = UITabBarItem(tabBarSystemItem: .resent, tag: 0)
-          homeVC.tabBarItem = icon1
-          let controllers = [homeVC]  //array of the root view controllers displayed by the tab bar interface
-          self.viewControllers = controllers
-      }
-
+    private func setupUI() {
+        view.backgroundColor = .white
+        self.tabBar.tintColor = .black
+    }
 }

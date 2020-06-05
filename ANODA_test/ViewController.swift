e8249e4e908a415fd73627a58a56df89baba202f
Taras Tarasenko <myavailability.taras@gmail.com>
2020-06-05 21:50:16 +0300

    init commit

---
+++ ANODA_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ANODA_test
+//
+//  Created by md760 on 05.06.2020.
+//  Copyright © 2020 md760. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ANODA_testTests/ANODA_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  ANODA_testTests.swift
+//  ANODA_testTests
+//
+//  Created by md760 on 05.06.2020.
+//  Copyright © 2020 md760. All rights reserved.
+//
+
+import XCTest
+@testable import ANODA_test
+
+class ANODA_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
